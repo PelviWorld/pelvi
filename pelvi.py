7a28a7b29012330f83fdc82d92a5959f56401dd4
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-10-06 12:18:47 +0200

    creating new user in pelvi class

--- pelvi.py
+++ pelvi.py
@@ -4,13 +4,16 @@ class Pelvi:
     def __init__(self):
         self.__pelvidata = Pelvidata()
         self.__user = self.__pelvidata.get_default_user()
-        self.__load_user_data(self.__user.userid)
         self.__device_list = self.__pelvidata.get_device_list()
+        self.__load_user_data(self.__user.userid)
 
     def __load_user_data(self, userid):
         self.__position_list = self.__pelvidata.get_position_list(userid)
         self.__blocked_list = self.__pelvidata.get_blocked_list(userid)
         self.__device_axis_list = self.__pelvidata.get_device_axis_list()
+
+    def load_user(self, name, lastname):
+        return self.__pelvidata.load_user(name, lastname)
 
     @property
     def user(self):
@@ -28,27 +31,52 @@ class Pelvi:
     def blocked_list(self):
         return self.__blocked_list
 
+    def add_new_user(self, name, lastname):
+        if not self.__pelvidata.check_user_name_not_in_use(name, lastname):
+            userid = self.load_user(name, lastname)
+            self.__load_user_data(userid)
+            return
+        self.__user.name = name
+        self.__user.lastname = lastname
+        self.__user.userid = self.__pelvidata.add_new_user(name, lastname)
+
+        for position in self.__position_list:
+            position[0].userid = self.__user.userid
+            positionsid = self.__pelvidata.add_new_positions_head(position[0])
+            for position_in_positionlist in position[2]:
+                position_in_positionlist.positionsid = positionsid
+                self.__pelvidata.add_new_position(position_in_positionlist)
+
+        self.__pelvidata.add_new_blocked_area(self.__user.userid, self.__blocked_list)
+
     def axis_name_for_device_axisid(self, device_axis_id):
         return self.__device_axis_list[device_axis_id]
+
+    def print_user_data(self):
+        print("USER:", self.user.name, self.user.lastname)
+        print("DEVICES:")
+        for device in self.device_list:
+            print(device.devicename)
+            print("DEVICEAXISES:")
+            for axis in device.axislist:
+                print(axis.axisname, axis.minvalue, axis.maxvalue, axis.refvalue)
+        print("POSITIONS:")
+        for positions in self.position_list:
+            print("Positionid:", positions[0].positionsid, "number:", positions[0].positionsnumber)
+            for power in positions[1]:
+                print("Device", power['deviceid'], power['power'])
+            for axis_position in positions[2]:
+                print("Axis", self.axis_name_for_device_axisid(axis_position.deviceaxisid), "Position",
+                      axis_position.position)
+
+        print("BLOCKS:")
+        for blocked in self.blocked_list:
+            print("Blockvalue Axis:", blocked.axis, "Min Value:", blocked.minvalue, "Max Value", blocked.maxvalue)
+
 
 
 if __name__ == '__main__':
     pelvi = Pelvi()
-    print("USER:", pelvi.user.name, pelvi.user.lastname)
-    print("DEVICES:")
-    for device in pelvi.device_list:
-        print(device.devicename)
-        print("DEVICEAXISES:")
-        for axis in device.axislist:
-            print(axis.axisname, axis.minvalue, axis.maxvalue, axis.refvalue)
-    print("POSITIONS:")
-    for positions in pelvi.position_list:
-        print("Positionid:", positions[0].positionsid, "number:", positions[0].positionsnumber)
-        for power in positions[1]:
-            print("Device", power['deviceid'], power['power'])
-        for axis_position in positions[2]:
-            print("Axis", pelvi.axis_name_for_device_axisid(axis_position.deviceaxisid), "Position", axis_position.position)
-
-    print("BLOCKS:")
-    for blocked in pelvi.blocked_list:
-        print("Blockvalue Axis:", blocked.axis, "Min Value:", blocked.minvalue, "Max Value", blocked.maxvalue)
+    pelvi.print_user_data()
+    pelvi.add_new_user("Test", "Name")
+    pelvi.print_user_data()
