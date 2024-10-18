d1b0cccaecc90b4a8d41536ab7a5e046fb8a83d0
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-10-18 16:25:11 +0200

    remove the try catch for the images to make sure we have background graphics

--- pelvi/background.py
+++ pelvi/background.py
@@ -1,19 +1,8 @@
-# Prüfen, ob Pillow installiert ist
-try:
-    from PIL import Image, ImageTk
-except ImportError:
-    print("Pillow ist nicht installiert. Bitte installieren Sie es mit 'pip install Pillow'.")
-    Image = None
+from PIL import Image, ImageTk
 
 def load_image_to_canvas(canvas, image_path, width, height):
-    if Image:
-        try:
-            img = Image.open(image_path)
-            img = img.resize((width, height))
-            bg = ImageTk.PhotoImage(img)
-            canvas.create_image(0, 0, image=bg, anchor='nw')
-            canvas.image = bg  # Keep a reference to avoid garbage collection
-        except Exception as e:
-            print(f"Konnte das Bild {image_path} nicht laden: {e}")
-    else:
-        print("Pillow ist nicht installiert. Kein Hintergundbild für die Leinwand möglich.")
+    img = Image.open(image_path)
+    img = img.resize((width, height))
+    bg = ImageTk.PhotoImage(img)
+    canvas.create_image(0, 0, image=bg, anchor='nw')
+    canvas.image = bg  # Keep a reference to avoid garbage collection
