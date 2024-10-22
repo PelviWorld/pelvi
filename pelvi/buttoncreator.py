40c5f8c64ee32029c25d52188a1c135127e80bc2
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-10-22 16:58:22 +0200

    remove duplicate MOTOR in motor command

--- pelvi/buttoncreator.py
+++ pelvi/buttoncreator.py
@@ -71,11 +71,11 @@ def create_canvas_dc_motor_buttons(canvas_frame, arduino):
     frame_motor.grid(row=3, column=0, pady=10)
     label_motor = ttk.Label(frame_motor, text="DC-Motor-Steuerung")
     label_motor.grid(row=3, column=0, columnspan=3)
-    btn_motor_reverse = ttk.Button(frame_motor, text="Motor Vorwärts", command=lambda: motor_command(arduino, 'MOTOR FORWARD'))
+    btn_motor_reverse = ttk.Button(frame_motor, text="Motor Vorwärts", command=lambda: motor_command(arduino, 'FORWARD'))
     btn_motor_reverse.grid(row=4, column=0, padx=3)
-    btn_motor_stop = ttk.Button(frame_motor, text="Motor Stop", command=lambda: motor_command(arduino, 'MOTOR STOP'))
+    btn_motor_stop = ttk.Button(frame_motor, text="Motor Stop", command=lambda: motor_command(arduino, 'STOP'))
     btn_motor_stop.grid(row=5, column=0, columnspan=2, pady=4, padx=3)
-    btn_motor_forward = ttk.Button(frame_motor, text="Motor Rückwärts", command=lambda: motor_command(arduino, 'MOTOR REVERSE'))
+    btn_motor_forward = ttk.Button(frame_motor, text="Motor Rückwärts", command=lambda: motor_command(arduino, 'REVERSE'))
     btn_motor_forward.grid(row=4, column=1, padx=3)
 
 def create_canvas_save_button(canvas_frame, pelvi):
@@ -84,7 +84,7 @@ def create_canvas_save_button(canvas_frame, pelvi):
 
 def motor_command(arduino, command):
     arduino.send_coordinates('MOTOR', command)
-    print(f"DC Motor-Befehl: {command}")
+    print(f"DC Motor-Befehl: MOTOR {command}")
 
 def save_data(pelvi):
     pelvi.save_user_data()
