38dc67707db3882baae3bc9d18bdffd5d55bdc88
Volker Wengert <22374206+vwengert@users.noreply.github.com>
2024-10-24 15:06:27 +0200

    print out serial response from arduino in separate thread

--- pelvi/arduino.py
+++ pelvi/arduino.py
@@ -1,14 +1,40 @@
 import time
 import serial
+import threading
 
 class ArduinoMock:
-    @staticmethod
-    def write(data):
-        print(f"Gesendet (Mock): {data.decode().strip()}")
+    def __init__(self):
+        self.buffer = []
+
+    def write(self, data):
+        command = data.decode().strip()
+        if command == 'HOMING':
+            self.buffer.append(b'Homing complete\n')
+        elif command.startswith('MOTOR '):
+            self.buffer.append(f'Motor command received {command}\n'.encode())
+        elif command.startswith('X '):
+            self.buffer.append(b'X axis moved\n')
+        elif command.startswith('Y '):
+            self.buffer.append(b'Y axis moved\n')
+        elif command.startswith('Z '):
+            self.buffer.append(b'Z axis moved\n')
+        elif command.startswith('E0 '):
+            self.buffer.append(b'E0 axis moved\n')
+        elif command.startswith('E1 '):
+            self.buffer.append(b'E1 axis moved\n')
 
     @staticmethod
     def close():
         print("Mock-Serial-Verbindung geschlossen.")
+
+    @property
+    def in_waiting(self):
+        return len(self.buffer)
+
+    def readline(self):
+        if self.buffer:
+            return self.buffer.pop(0)
+        return b''
 
 class Arduino:
     def __init__(self, port, baudrate=115200):
@@ -17,6 +43,7 @@ class Arduino:
         self.__serial = None
         self.__serial_connection_timeout = 2
         self.__connect()
+        self.__start_serial_thread()
 
     def __del__(self):
         if self.__serial is not None:
@@ -47,3 +74,15 @@ class Arduino:
             print(f"Gesendet: {command.strip()}")
         else:
             print("Arduino ist nicht verbunden.")
+
+    def __start_serial_thread(self):
+        self.__serial_thread = threading.Thread(target=self.__read_from_serial)
+        self.__serial_thread.daemon = True
+        self.__serial_thread.start()
+
+    def __read_from_serial(self):
+        while True:
+            if self.__serial.in_waiting > 0:
+                line = self.__serial.readline().decode('utf-8').rstrip()
+                print(line)
+            time.sleep(0.1)  # Small delay to prevent high CPU usage
